436282a05476b0c86cf5998f448b143de73709fd
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2014-11-28 16:12:56 +0500

    explore chapter 11

--- SpringInAction/src/main/java/ru/vsprog/springinaction/chapter7/SpitterSeviceImpl.java
+++ SpringInAction/src/main/java/ru/vsprog/springinaction/chapter7/SpitterSeviceImpl.java
@@ -1,20 +1,31 @@
 package ru.vsprog.springinaction.chapter7;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
+import ru.vsprog.springinaction.chapter6.Spitter;
 import ru.vsprog.springinaction.chapter6.SpitterDAO;
+import ru.vsprog.springinaction.chapter8.SpitterService;
 
 import javax.persistence.EntityTransaction;
+import java.util.List;
 
 /**
  * Created by vsa
  * Date: 19.11.14.
  */
-public class SpitterSeviceImpl {
+public class SpitterSeviceImpl implements SpitterService {
     private SpitterDAO spitterDao;
     private EntityTransaction txStatus;
     private TransactionTemplate transactionTemplate;
+    @Autowired
+    SpitterService spitterService;
+
+    @Override
+    public List<Spittle> getRecentSpittles(int count) {
+        return null;
+    }
 
     public void saveSpittle(final Spittle spittle) {
         transactionTemplate.execute(new TransactionCallback<Void>() {
@@ -29,6 +40,52 @@ public class SpitterSeviceImpl {
                 return null;
             }
         });
+    }
+
+    @Override
+    public void saveSpitter(Spitter spitter) {
+
+    }
+
+    @Override
+    public Spitter getSpitter(long id) {
+        return null;
+    }
+
+    @Override
+    public void startFollowing(Spitter follower, Spitter followee) {
+
+    }
+
+    @Override
+    public List<Spittle> getSpittlesForSpitter(Spitter spitter) {
+        return null;
+    }
+
+    @Override
+    public List<Spittle> getSpittlesForSpitter(String username) {
+        Spitter spitter = spitterService.getSpitter(username);
+        return spitterService.getSpittlesForSpitter(spitter);
+    }
+
+    @Override
+    public Spitter getSpitter(String username) {
+        return null;
+    }
+
+    @Override
+    public Spittle getSpittleById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteSpittle(long id) {
+
+    }
+
+    @Override
+    public List<Spitter> getAllSpitters() {
+        return null;
     }
 
     public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
--- SpringInAction/src/main/java/ru/vsprog/springinaction/chapter8/SpitterService.java
+++ SpringInAction/src/main/java/ru/vsprog/springinaction/chapter8/SpitterService.java
@@ -1,14 +1,28 @@
 package ru.vsprog.springinaction.chapter8;
 
 import ru.vsprog.springinaction.chapter6.Spitter;
+import ru.vsprog.springinaction.chapter7.Spittle;
+
+import java.util.List;
 
 /**
  * Created by vsa
  * Date: 20.11.14.
  */
 public interface SpitterService {
-    Object getRecentSpittles(int defaultSpittlesPerPage);
-    Spitter getSpitter(String username);
-    Object getSpittlesForSpitter(String username);
+    List<Spittle> getRecentSpittles(int count);
+    void saveSpittle(Spittle spittle);
+
     void saveSpitter(Spitter spitter);
+    Spitter getSpitter(long id);
+    void startFollowing(Spitter follower, Spitter followee);
+
+    List<Spittle> getSpittlesForSpitter(Spitter spitter);
+    List<Spittle> getSpittlesForSpitter(String username);
+    Spitter getSpitter(String username);
+
+    Spittle getSpittleById(long id);
+    void deleteSpittle(long id);
+
+    List<Spitter> getAllSpitters();
 }
