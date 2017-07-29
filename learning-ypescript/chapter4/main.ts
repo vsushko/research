24d185896db6592f6e88761ceea8391358497bef
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-07-29 22:52:05 +0600

    renaming

--- learning-ypescript/chapter4/main.ts
+++
@@ -1,6 +0,0 @@
-var user = new app.models.UserModel();
-var talk = new app.models.TalkModel();
-var userValidator = new app.validation.UserValidator();
-var talkValidator = new app.validation.TalkValidator();
-
-import TalkValidatorAlias = app.validation.TalkValidator
