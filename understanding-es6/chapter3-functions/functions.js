06e9008723745d006eb67939df90fe978c869434
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-08-03 08:30:28 +0500

    add more examples

--- understanding-es6/chapter3-functions/functions.js
+++ understanding-es6/chapter3-functions/functions.js
@@ -82,3 +82,137 @@ function mixArgs(first, second = "b") {
 mixArgs("a");
 
 // default parameter expressions
+// arrow functions
+// arrow functions are functions defined with an arrow (=>)
+// behaviour difference:
+// - no this, super, arguments, and new.target bindings
+// - cannot be called with new (do not have a [[Construct]] method)
+// - no prototype (because you can't use new on an arrow functions)
+// - can't change this (the value of this inside the function can't be changed)
+// - no arguments object (because arrow functions have no arguments binding)
+// - no duplicate named parameters (arrow functions cannot have duplicate named 
+//   parameters in strict or non-strict mode)
+
+// arrow functions also have a name property that follows the same rule as other functions
+
+// arrow functions syntax
+
+// takes a single argument and simply returns it
+let reflect = value => value;
+
+// effectively equivalent to
+let reflect = function (value) {
+  return value;
+};
+
+// more than one argument
+let sum = (num1, num2) => num1 + num2;
+
+// effectively equivalent to
+let sum = function (num1, num2) {
+  return num1 + num2;
+}
+
+// if there are no arguments to the function, we must include an
+// empty set of parentheses in the declaraion
+let getName = () => "Nicholas";
+
+// effectively equivalent to
+let getName = function () {
+  return "Nicholas";
+}
+
+// wrap the function in curly braces when you need more than one expression
+let sum = (num1, num2) => {
+  return num1 + num2;
+}
+
+// effectively equivalent to
+let sum = function (num1, num2) {
+  return num1 + num2;
+}
+
+// creating a function that does nothing
+let doNothing = () => { };
+
+// effectively equivalent to
+let doNothing = function () { };
+
+// when we want to retrun an object literal outside a function
+// body must wrap the literal in parentheses
+let getTempItem = id => ({ id: id, name: "Temp" });
+
+// effectively equivalent to
+// wrapping the object literal in parentheses signals that the
+// curly braces are an obejct literal instead of the function body
+let getTempItem = function (id) {
+  return {
+    id: id,
+    name: "Temp"
+  }
+}
+
+// creating immediately invoked function expression
+// IIFEs  allow you to define an anonymous function and call 
+// it immediately without savin a reference.
+// This pattern comes in handy when you want to create a scope 
+// that is shielded from the rest of a program
+
+let person = function (name) {
+  return {
+    getName: function () {
+      return name;
+    }
+  };
+}("Nicholas");
+
+console.log(person.getName()); // "Nicholas"
+
+// using arrow function
+let person = ((name) => {
+  return {
+    getName: function () {
+      return name;
+    }
+  };
+})("Nicholas");
+
+console.log(person.getName()); // "Nicholas"
+
+// no this binding
+let PageHandler = {
+  id: "123456",
+  init: function () {
+    document.addEventListener("click", function (event) {
+      this.doSomething(event.type);  // error
+    }, false);
+  },
+  doSomething: function (type) {
+    console.log("Handling " + type + " for " + this.id);
+  }
+};
+
+// with binging which solving the problem
+let PageHandler = {
+  id: "123456",
+  init: function () {
+    document.addEventListener("click", (function (event) {
+      this.doSomething(event.type); // no error
+    }).bind(this), false);
+  },
+  doSomething: function (type) {
+    console.log("Handling " + type + " for " + this.id);
+  }
+}
+
+// with arrow function
+let PageHandler = {
+  id: "123456",
+  init: function () {
+    document.addEventListener("click", event =>
+      this.doSomething(event.type), false);
+  },
+  doSomething: function (type) {
+    console.log("Handling " + type + " for " + this.id);
+  }
+}
