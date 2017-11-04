32b42d2d293bd5e8439f81353bf52751b0004f7f
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-04 22:06:11 +0500

    add section folders

--- angular-4-the-complete-guide/basics-components/src/app/app.component.ts
+++ angular-4-the-complete-guide/basics-components/src/app/app.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length+1);
+    this.log.push(new Date());
+  }
 }
