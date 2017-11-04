104fbb969c5fbf7e45c525aae273699ca8eebe49
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-04 19:07:54 +0500

    add first assignment solution

--- angular-4-the-complete-guide/basics-components/src/app/warning-alert/warning-alert.component.ts
+++ angular-4-the-complete-guide/basics-components/src/app/warning-alert/warning-alert.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-warning-alert',
+  selector: '.app-warning-alert',
   templateUrl: './warning-alert.component.html',
   styleUrls: ['./warning-alert.component.css']
 })
