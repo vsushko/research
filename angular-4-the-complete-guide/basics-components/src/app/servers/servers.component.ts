0e613df8c5f3f81ceacae105933844b8c965ef33
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-04 20:34:34 +0500

    second assignment

--- angular-4-the-complete-guide/basics-components/src/app/servers/servers.component.ts
+++ angular-4-the-complete-guide/basics-components/src/app/servers/servers.component.ts
@@ -4,14 +4,28 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html'
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer: boolean = false;
+  serverName: string = 'Testserver';
+  serverCreationStatus: string = 'No server was created!';
+  
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
+
+   onCreateServer() {
+     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+   }
+   onUpdateServerName(event: Event) {
+     this.serverName = (<HTMLInputElement>event.target).value;
+
+   }
 
   ngOnInit() {
   }
