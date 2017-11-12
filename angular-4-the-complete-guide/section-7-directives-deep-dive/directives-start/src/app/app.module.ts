ba92cd67cc34419711dda10c745d8741249b293f
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-12 23:50:22 +0600

    understanding ngSwitch

--- angular-4-the-complete-guide/section-7-directives-deep-dive/directives-start/src/app/app.module.ts
+++ angular-4-the-complete-guide/section-7-directives-deep-dive/directives-start/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
