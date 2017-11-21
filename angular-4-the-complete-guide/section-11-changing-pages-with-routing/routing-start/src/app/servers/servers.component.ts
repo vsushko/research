8f71dd88a6867f07a75dc1a882399c847df031d7
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-22 03:25:05 +0600

    fetching route parameters reactively

--- angular-4-the-complete-guide/section-11-changing-pages-with-routing/routing-start/src/app/servers/servers.component.ts
+++ angular-4-the-complete-guide/section-11-changing-pages-with-routing/routing-start/src/app/servers/servers.component.ts
@@ -1,3 +1,4 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
 
@@ -9,10 +10,15 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    // this.router.navigate(['servers'], {relativeTo: this.route});
+  }
 }
