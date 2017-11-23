b02ee0aecb72efa4c07f0c7aecde5e366e7d5d1f
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-23 23:36:34 +0600

    resolving dynamic data with the resolve guard

---
+++ angular-4-the-complete-guide/section-11-changing-pages-with-routing/routing-start/src/app/error-page/error-page.component.ts
@@ -0,0 +1,25 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router/src/config';
+
+@Component({
+  selector: 'app-error-page',
+  templateUrl: './error-page.component.html',
+  styleUrls: ['./error-page.component.css']
+})
+export class ErrorPageComponent implements OnInit {
+
+  errorMessage: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // this.errorMessage = this.route.snapshot.data['message'];
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.errorMessage = data['message'];
+      }
+    );
+  }
+
+}
---
+++ angular-4-the-complete-guide/section-11-changing-pages-with-routing/routing-start/src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -0,0 +1,15 @@
+import { Observable } from 'rxjs/Observable';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+export interface CanComponentDeactive {
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+}
+
+export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactive> {
+
+  canDeactivate(component: CanComponentDeactive,
+    currentRoute: ActivatedRouteSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return component.canDeactivate();
+  }
+}
