ae46829c565d788771565065bd07b5a5f872c5ae
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-28 01:55:54 +0600

    complete section 13

--- angular-4-the-complete-guide/section-13-understanding-observables/observables-start/src/app/user/user.component.ts
+++ angular-4-the-complete-guide/section-13-understanding-observables/observables-start/src/app/user/user.component.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './../users.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
@@ -9,15 +10,20 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private usersService: UsersService) { }
 
   ngOnInit() {
     this.route.params
       .subscribe(
-        (params: Params) => {
-          this.id = +params['id'];
-        }
+      (params: Params) => {
+        this.id = +params['id'];
+      }
       );
+  }
+
+  onActivate() {
+    this.usersService.userActivated.next(this.id);
   }
 
 }
---
+++ angular-4-the-complete-guide/section-13-understanding-observables/observables-start/src/app/users.service.ts
@@ -0,0 +1,6 @@
+import { Subject } from 'rxjs/Subject';
+
+export class UsersService {
+  userActivated = new Subject();
+
+}
