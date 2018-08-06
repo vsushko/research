ed86a4b1eeaf56a808a3a7f339e152f7a0c1aa4b
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-08-06 21:22:12 +0300

    added join, sleep, yield

---
+++ java-playground/java-playground/src/main/java/vsushko/geeksforgeeks/multithreading/JoinDemo.java
@@ -0,0 +1,30 @@
+package vsushko.geeksforgeeks.multithreading;
+
+/**
+ * @author Vasiliy Sushko
+ */
+public class JoinDemo implements Runnable {
+
+    @Override
+    public void run() {
+        Thread t = Thread.currentThread();
+        System.out.println("Current thread: " + t.getName());
+
+        // checks if current thread is alive
+        System.out.println("Is Alive? " + t.isAlive());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new JoinDemo());
+        t.start();
+
+        // waits for 1000ms this thread to die
+        t.join(1000);
+
+        System.out.println("\nJoining after 1000 mili seconds: \n");
+        System.out.println("Current thread: " + t.getName());
+
+        // checks if this thread is alive
+        System.out.println("Is alive? " + t.isAlive());
+    }
+}
---
+++ java-playground/java-playground/src/main/java/vsushko/geeksforgeeks/multithreading/MyThreadWithYield.java
@@ -0,0 +1,28 @@
+package vsushko.geeksforgeeks.multithreading;
+
+/**
+ * @author Vasiliy Sushko
+ */
+public class MyThreadWithYield extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " in control");
+        }
+    }
+}
+
+class YieldDemo {
+    public static void main(String[] args) {
+        MyThreadWithYield t = new MyThreadWithYield();
+        t.start();
+
+        for (int i = 0; i < 5; i++) {
+            // Control passes to child thread
+            Thread.yield();
+            // After execution of child Thread main thread takes over
+            System.out.println(Thread.currentThread().getName() + " in control");
+        }
+    }
+}
