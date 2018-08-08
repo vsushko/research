253a8cd2ad9e0b9a3aef1114487c9923d0585d1e
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-08-08 20:00:41 +0300

    added concurrency samples

---
+++ java-playground/java-playground/src/main/java/vsushko/geeksforgeeks/multithreading/ChildAndParentThreadPriority.java
@@ -0,0 +1,26 @@
+package vsushko.geeksforgeeks.multithreading;
+
+/** Java program to demonstrate that a child thread gets same priority as parent
+ *
+ * @author Vasiliy Sushko
+ */
+public class ChildAndParentThreadPriority extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("Inside run method");
+    }
+
+    public static void main(String[] args) {
+        // main thread priority is 6 not
+        Thread.currentThread().setPriority(6);
+
+        System.out.println("main thread priority : " +
+                Thread.currentThread().getPriority());
+
+        ChildAndParentThreadPriority t1 = new ChildAndParentThreadPriority();
+
+        // t1 thread is a child of main thread so t1 thread will also have priority 6
+        System.out.println("t1 thread priority: " + t1.getPriority());
+    }
+}
