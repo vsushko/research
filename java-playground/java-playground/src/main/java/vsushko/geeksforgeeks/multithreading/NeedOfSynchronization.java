06ef9f27bcbf061b070e07adaee17ad351d8cfa3
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-08-10 23:11:52 +0300

    added examples

---
+++ java-playground/java-playground/src/main/java/vsushko/geeksforgeeks/multithreading/NeedOfSynchronization.java
@@ -0,0 +1,29 @@
+package vsushko.geeksforgeeks.multithreading;
+
+
+/**
+ * Java program to illustrate need of Synchronization
+ *
+ * @author Vasiliy Sushko
+ */
+public class NeedOfSynchronization {
+
+    public static void main(String[] args) {
+        MultiThreadTwo t = new MultiThreadTwo();
+        t.increment();
+        System.out.println(t.getValue());
+    }
+}
+
+class MultiThreadTwo {
+    private int i = 0;
+
+    public void increment() {
+        i++;
+    }
+
+    public int getValue() {
+        return i;
+    }
+}
+
