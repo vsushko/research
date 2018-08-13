3734bd8b9c3721d08b261f40c4e17c4346ad6b48
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-08-13 19:58:33 +0300

    added ping pong solution

---
+++ java-playground/java-playground/src/main/java/vsushko/concurrent/PingPongDemo.java
@@ -0,0 +1,48 @@
+package vsushko.concurrent;
+
+/**
+ * @author Vasiliy Sushko
+ */
+public class PingPongDemo {
+
+    public static void main(String[] args) {
+        PingPongPrinter printer = new PingPongPrinter();
+        PingPongThread t1 = new PingPongThread(printer, "Ping");
+        PingPongThread t2 = new PingPongThread(printer, "Pong");
+        t1.start();
+        t2.start();
+    }
+}
+
+class PingPongThread extends Thread {
+    private final PingPongPrinter printer;
+    private final String s;
+
+    public PingPongThread(PingPongPrinter printer, String s) {
+        this.printer = printer;
+        this.s = s;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (printer) {
+                printer.printString(s);
+                printer.notify();
+
+                try {
+                    printer.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+class PingPongPrinter {
+    public void printString(String s) {
+        System.out.println(s);
+    }
+}
+
