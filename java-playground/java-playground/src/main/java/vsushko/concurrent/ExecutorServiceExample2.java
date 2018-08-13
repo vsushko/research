bd53678cf123156ae67afaa24dc2be37c51a96b5
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-08-13 20:08:28 +0300

    added executor service examples

---
+++ java-playground/java-playground/src/main/java/vsushko/concurrent/ExecutorServiceExample2.java
@@ -0,0 +1,27 @@
+package vsushko.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Vasiliy Sushko
+ */
+public class ExecutorServiceExample2 {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        executor.submit(() -> new Task() {
+            @Override
+            public void run() {
+                System.out.println("Task2");
+            }
+        }.run());
+    }
+}
+
+class Task implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Task1");
+    }
+}
