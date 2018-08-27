e1e6d3c7f0d41a6d0479e4143b5d85a345859fe4
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-08-27 21:35:45 +0300

    added client and server examples with ssl tls

---
+++ java-playground/java-playground/src/main/java/vsushko/networking/ClientWithCommonCert.java
@@ -0,0 +1,64 @@
+package vsushko.networking;
+
+import javax.net.ssl.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+
+/**
+ * @author vsushko
+ */
+public class ClientWithCommonCert {
+
+    private static final String passphrase = "clientpw";
+
+    // constructor to put ip address and port
+    public ClientWithCommonCert(String address, int port) throws GeneralSecurityException, IOException {
+
+        SecureRandom secureRandom = new SecureRandom();
+
+        // client
+        KeyStore clientKeyStore = KeyStore.getInstance("JKS");
+        clientKeyStore.load(new FileInputStream("server.private"), passphrase.toCharArray());
+
+        // server
+        KeyStore serverKeyStore = KeyStore.getInstance("JKS");
+        serverKeyStore.load(new FileInputStream("server.public"), "public".toCharArray());
+
+        //
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(serverKeyStore);
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(clientKeyStore, passphrase.toCharArray());
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), secureRandom);
+
+        // establish a connection
+        SSLSocketFactory sf = sslContext.getSocketFactory();
+        SSLSocket socket = (SSLSocket) sf.createSocket(address, port);
+        System.out.println("Connected");
+
+        DataInputStream din = new DataInputStream(System.in);
+        DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+
+        String input = "";
+        try {
+            while (!"quit".equals(input)) {
+                input = din.readLine();
+                dout.writeUTF(input);
+            }
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) throws GeneralSecurityException, IOException {
+        ClientWithCommonCert client = new ClientWithCommonCert("127.0.0.1", 8181);
+    }
+}
---
+++ java-playground/java-playground/src/main/java/vsushko/networking/GFGClient.java
@@ -0,0 +1,62 @@
+package vsushko.networking;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * @author vsushko
+ */
+public class GFGClient {
+    // initialize socket and input output streams
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+    // constructor to put ip address and port
+    public GFGClient(String address, int port) {
+        // establish a connection
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            input = new DataInputStream(System.in);
+
+            // sends output to the socket
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException u) {
+            System.out.println(u);
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+
+        // string to read message from input
+        String line = "";
+
+        // keep reading until "Over" is input
+        while (!line.equals("Over")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        // close the connection
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        GFGClient client = new GFGClient("127.0.0.1", 5000);
+    }
+}
