e88b2d8172c2fcd86439f8321ae9262c5a959b73
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-09-08 16:31:20 +0300

    added singleton patterns

---
+++ java-playground/java-playground/src/main/java/vsushko/patterns/singleton/IvoryTower.java
@@ -0,0 +1,11 @@
+package vsushko.patterns.singleton;
+
+public final class IvoryTower {
+    private IvoryTower() {}
+
+    private static final IvoryTower INSTANCE = new IvoryTower();
+
+    public static IvoryTower getInstance() {
+        return INSTANCE;
+    }
+}
