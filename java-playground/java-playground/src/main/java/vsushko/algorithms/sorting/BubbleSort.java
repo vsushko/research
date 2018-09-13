9125fba4e5ec00959e1623a202fbd9eef0066518
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-09-14 00:57:08 +0300

    added bubble sort optimized solution

--- java-playground/java-playground/src/main/java/vsushko/algorithms/sorting/BubbleSort.java
+++ java-playground/java-playground/src/main/java/vsushko/algorithms/sorting/BubbleSort.java
@@ -11,13 +11,12 @@ class BubbleSort {
 
     public static void main(String args[]) {
         int arr[] = {64, 34, 25, 12, 22, 11, 90};
-        bubbleSort(arr);
+        bubbleSort(arr, arr.length);
         System.out.println("Sorted array");
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void bubbleSort(int arr[]) {
-        int n = arr.length;
+    private static void bubbleSort(int[] arr, int n) {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
