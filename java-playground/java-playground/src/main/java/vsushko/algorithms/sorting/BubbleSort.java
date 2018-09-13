cee626a274c413d4a68991af0271cb0f75e53b6d
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-09-14 00:44:29 +0300

    added bubble sort

---
+++ java-playground/java-playground/src/main/java/vsushko/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,32 @@
+package vsushko.algorithms.sorting;
+
+import java.util.Arrays;
+
+/**
+ * Java program for implementation of Bubble Sort
+ *
+ * @author vsushko
+ */
+class BubbleSort {
+
+    public static void main(String args[]) {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println("Sorted array");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
