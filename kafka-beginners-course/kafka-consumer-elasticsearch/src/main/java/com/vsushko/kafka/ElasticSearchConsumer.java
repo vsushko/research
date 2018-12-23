442366565efeeb1a359f2ecd0d0ff2d735ae903f
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-12-23 15:19:51 +0300

    elasticsearch consumer fixes

--- kafka-beginners-course/kafka-consumer-elasticsearch/src/main/java/com/vsushko/kafka/ElasticSearchConsumer.java
+++ kafka-beginners-course/kafka-consumer-elasticsearch/src/main/java/com/vsushko/kafka/ElasticSearchConsumer.java
@@ -3,20 +3,16 @@ package com.vsushko.kafka;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
@@ -44,65 +40,60 @@ public class ElasticSearchConsumer {
             // new in Kafka 2.0.0
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
-            LOGGER.info("Received " + records.count() + " records");
+            int recordCount = records.count();
+            LOGGER.info("Received " + recordCount + " records");
+
+            BulkRequest bulkRequest = new BulkRequest();
 
             for (ConsumerRecord<String, String> record : records) {
-
-                // 2 strategies
-                // kafka generic ID
-//                 String id = record.topic() + "_" + record.partition() + "_" + record.offset();
+                String recordData = record.value();
                 // twitter feed specific id
-                String id = extractIdFromTweet(record.value());
+                String id = extractIdFromTweet(recordData);
 
-                // insert data into ElasticSearch
-                String jsonString = retrieveMessage(record.value());
-                if (jsonString != null) {
+                try {
+                    // insert data into ElasticSearch
+                    String jsonString = retrieveMessage(recordData);
+                    if (jsonString.isEmpty()) {
+                        continue;
+                    }
                     IndexRequest indexRequest =
                             new IndexRequest("twitter", "tweets", id)
                                     .source(jsonString, XContentType.JSON);
-
-                    IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-                    LOGGER.info(indexResponse.getId());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    LOGGER.warn("Received record with id: " + id);
+                    // we add to our bulk request (takes no time)
+                    bulkRequest.add(indexRequest);
+                } catch (NullPointerException e) {
+                    LOGGER.warn("skipping bad data: " + recordData);
                 }
             }
-            LOGGER.info("Committing offsets...");
-            consumer.commitSync();
-            LOGGER.info("Offsets have been committed");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if (recordCount > 0) {
+                BulkResponse bulkItemResponses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                LOGGER.info("Committing offsets...");
+                consumer.commitSync();
+                LOGGER.info("Offsets have been committed");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
-
-        // close the client gracefully
+        // close the client
         // client.close();
     }
 
     private static RestHighLevelClient createClient() {
-        // credentials
-        String hostname = "";
-        String username = "";
-        String password = "";
-
-        // for the remote ES
-        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
-
-        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, 443, "https"))
-                .setHttpClientConfigCallback(httpAsyncClientBuilder ->
-                        httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
-
-        return new RestHighLevelClient(builder);
+        return new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
     }
 
     private static String extractIdFromTweet(String tweetJson) {
-        return jsonParser.parse(tweetJson).getAsJsonObject().get("id_str").getAsString();
+        String result = "";
+        try {
+            result = jsonParser.parse(tweetJson).getAsJsonObject().get("id_str").getAsString();
+        } catch (JsonSyntaxException e) {
+            LOGGER.warn("Unterminated object");
+        }
+        return result;
     }
 
     private static String retrieveMessage(String value) {
@@ -110,7 +101,7 @@ public class ElasticSearchConsumer {
             jsonParser.parse(value);
         } catch (JsonSyntaxException ex) {
             LOGGER.error("Received not a valid message");
-            return null;
+            return "";
         }
         return value;
     }
@@ -123,15 +114,11 @@ public class ElasticSearchConsumer {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-
         // manual commit of offsets
         // disable auto commit of offsets
         properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
-
         // earliest - read from the beginning of the topic
-        // latest - only the new messages
-        // none - will throw an error if there is no offsets
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // create consumer
