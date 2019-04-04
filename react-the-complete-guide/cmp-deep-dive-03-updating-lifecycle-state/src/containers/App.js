5cd6ba8961f3f3e8607b6f3c249859d5b559c317
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-04-04 07:48:14 +0300

    added updating lifecycle state cmp example

--- react-the-complete-guide/cmp-deep-dive-03-updating-lifecycle-state/src/containers/App.js
+++ react-the-complete-guide/cmp-deep-dive-03-updating-lifecycle-state/src/containers/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+
 import classes from './App.css';
-import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,43 +12,37 @@ class App extends Component {
 
   state = {
     persons: [
-      { id: 'id1', name: 'Max', age: 28 },
-      { id: 'id2', name: 'Manu', age: 29 },
-      { id: 'id3', name: 'Stephanie', age: 26 }
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
-    doesShow: false
-  }
+    showPersons: false
+  };
 
   static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props)
+    console.log('[App.js] getDerivedStateFromProps', props);
     return state;
   }
+
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
 
   componentDidMount() {
     console.log('[App.js] componentDidMount');
   }
 
-  componentWillMount() {
-    console.log('[App.js] componentWillMount ');
-  }
-
-  componentDidUpdate() {
-    console.log("App.js: componentDidUpdate");
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
     return true;
   }
 
-  shouldComponentUpdate() {
-    console.log("App.js: shouldComponentUpdate");
-    return false;
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
   }
 
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  }
-
-  nameChangeHandler = (event, id) => {
+  nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
@@ -55,37 +51,50 @@ class App extends Component {
       ...this.state.persons[personIndex]
     };
 
-    // const person = Object.assign({ this.state.persons[personIndex] })
+    // const person = Object.assign({}, this.state.persons[personIndex]);
 
     person.name = event.target.value;
+
     const persons = [...this.state.persons];
-    persons[personIndex] = persons
+    persons[personIndex] = person;
 
-    this.setPersons({ persons: persons })
-  }
+    this.setState({ persons: persons });
+  };
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({ persons: persons })
-  }
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
-    console.log('[App.js] rendering...')
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
       persons = (
-        <Persons persons={this.state.persons} clicked={this.deletePersonHandler} changed={this.nameChangeHandler} />
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
       );
     }
+
     return (
       <div className={classes.App}>
-        <Cockpit title={this.props.appTitle}
+        <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
-          clicked={this.togglePersonsHandler} />
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
