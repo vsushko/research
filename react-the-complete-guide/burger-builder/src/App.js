b8eab80a1bfbe64cee2ca37f6b4df77dbd32e44e
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-04-13 17:01:27 +0300

    added burger builder project

--- react-the-complete-guide/burger-builder/src/App.js
+++ react-the-complete-guide/burger-builder/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import Layout from './hoc/Layout/Layout';
 
-import Layout from './components/Layout/Layout.js';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder.js';
 
 class App extends Component {
