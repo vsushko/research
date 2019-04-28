91097a52073117995392d124ce2f5f45b7dac4e8
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-04-28 23:02:20 +0300

    added improvements

--- react-the-complete-guide/burger-builder/src/index.js
+++ react-the-complete-guide/burger-builder/src/index.js
@@ -12,7 +12,8 @@ import thunk from 'redux-thunk';
 
 import './index.css';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
