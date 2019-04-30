8092574dd617372e3c797696c8d3522a9c375ce8
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-04-30 07:55:22 +0300

    added tests

--- react-the-complete-guide/burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ react-the-complete-guide/burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,8 +7,24 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = wrapper.find(NavigationItem)
+  })
+
   it('should render two <NavitationItem/> elements if not authenticated', () => {
     const wrapper = shallow(<NavigationItems />);
-    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    expect(wrapper).toHaveLength(2);
+  });
+
+  it('should render three <NavitationItem/> elements if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper).toHaveLength(3);
+  });
+
+  it('should render three <NavitationItem/> elements if authenticated1', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
   });
 });
