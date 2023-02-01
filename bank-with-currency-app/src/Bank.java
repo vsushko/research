e2cb7c9922c427946c9a860cc20ba41b81b1ae40
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-02-02 01:44:31 +0300

    added bootstrap frameork

--- bank-with-currency-app/src/Bank.java
+++ bank-with-currency-app/src/Bank.java
@@ -2,6 +2,7 @@ import java.util.Hashtable;
 
 public class Bank {
     private Hashtable rates = new Hashtable();
+
     Money reduce(Expression source, String to){
         return source.reduce(this, to);
     }
