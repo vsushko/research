38660d3435fe0885431a92a19813aff1c8044717
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-11-07 23:38:59 +0300

    props changes

--- react-the-complete-guide/01-starting-project/src/App.jsx
+++ react-the-complete-guide/01-starting-project/src/App.jsx
@@ -1,70 +1,17 @@
-import { useState } from 'react';
-
-import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-import { EXAMPLES } from './data.js';
+import Examples from './components/Examples/Examples.jsx';
+import CoreConcepts from './components/CoreConsepts/CoreConsepts.jsx';
 
 function App() {
-  const [selectedTopic, setTelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setTelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>Please select a topic</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              onSelect={() => handleSelect('components')}
-              isSelected={selectedTopic === 'components'}>
-              Components
-            </TabButton>
-            <TabButton
-              onSelect={() => handleSelect('jsx')}
-              isSelected={selectedTopic === 'jsx'}>
-              JSX
-            </TabButton>
-            <TabButton
-              onSelect={() => handleSelect('props')}
-              isSelected={selectedTopic === 'props'}>
-              Props
-            </TabButton>
-            <TabButton
-              onSelect={() => handleSelect('state')}
-              isSelected={selectedTopic === 'state'}>
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ react-the-complete-guide/01-starting-project/src/components/Examples/Examples.jsx
@@ -0,0 +1,60 @@
+
+import { useState } from 'react';
+
+import { EXAMPLES } from '../../data.js';
+import TabButton from '../TabButton.jsx';
+import Section from '../Section/Section.jsx';
+import Tabs from '../Tabs.jsx';
+
+export default function Examples() {
+  const [selectedTopic, setTelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setTelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              onClick={() => handleSelect('components')}
+              isSelected={selectedTopic === 'components'}>
+              Components
+            </TabButton>
+            <TabButton
+              onClick={() => handleSelect('jsx')}
+              isSelected={selectedTopic === 'jsx'}>
+              JSX
+            </TabButton>
+            <TabButton
+              onClick={() => handleSelect('props')}
+              isSelected={selectedTopic === 'props'}>
+              Props
+            </TabButton>
+            <TabButton
+              onClick={() => handleSelect('state')}
+              isSelected={selectedTopic === 'state'}>
+              State
+            </TabButton>
+          </>}>
+        {tabContent}
+      </Tabs>
+    </Section >
+  );
+}
