09c93b20b4c644de2a2f2e42a38e99e00c30bb01
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-12-19 23:33:31 +0300

    react context

--- react-the-complete-guide/elegant-context-project/src/App.jsx
+++ react-the-complete-guide/elegant-context-project/src/App.jsx
@@ -5,6 +5,7 @@ import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import { CartContext } from "./store/shopping-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -67,8 +68,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -80,7 +86,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
